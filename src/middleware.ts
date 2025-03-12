e6982d817f4d8b85adcb1fe47448883d44347470
PikkMoune71 <antoinepicard385@gmail.com>
2025-03-12 10:00:03 +0100

    fix: middleware redirect auth

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,31 +1,42 @@
 import { clerkMiddleware, createRouteMatcher } from "@clerk/nextjs/server";
 import { createI18nMiddleware } from "next-international/middleware";
-import { NextRequest } from "next/server";
 
-// Match les routes publiques
-const isPublicRoute = createRouteMatcher(["/sign-in(.*)", "/"]);
+// Définition des locales
+const locales = ["en", "fr"];
+const defaultLocale = "en";
 
-// Middleware Clerk
+// Middleware I18n
+const I18nMiddleware = createI18nMiddleware({
+  locales,
+  defaultLocale,
+});
+
+// Match les routes publiques (ajout des versions localisées)
+const isPublicRoute = createRouteMatcher([
+  "/sign-in(.*)",
+  "/",
+  "/fr",
+  "/en",
+  "/fr/sign-in(.*)",
+  "/en/sign-in(.*)",
+]);
+
 export default clerkMiddleware(async (auth, request) => {
+  // Exclure les API
+  if (request.nextUrl.pathname.startsWith("/api")) {
+    return;
+  }
+
+  // Appliquer la gestion des langues
+  const response = I18nMiddleware(request);
+
+  // Vérifier si la route est protégée
   if (!isPublicRoute(request)) {
     await auth.protect();
   }
+
+  return response;
 });
-
-// Créer un middleware I18n
-const I18nMiddleware = createI18nMiddleware({
-  locales: ["en", "fr"],
-  defaultLocale: "en",
-});
-
-export function middleware(request: NextRequest) {
-  // Exclure les API de la gestion des locales
-  if (request.nextUrl.pathname.startsWith("/api")) {
-    return; // Ne pas appliquer I18nMiddleware pour les API
-  }
-
-  return I18nMiddleware(request); // Appliquer I18nMiddleware pour toutes les autres routes
-}
 
 // Configurer le matcher
 export const config = {
